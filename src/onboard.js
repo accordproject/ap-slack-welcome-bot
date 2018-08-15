178e0702a8533ae5e0eaa18aba3b3ebb9044f087
Matt Roberts <code@rbrts.uk>
2018-08-15 19:52:44 +0100

    Update onboard.js

--- src/onboard.js
+++ src/onboard.js
@@ -11,26 +11,58 @@ const message = {
   token: process.env.SLACK_TOKEN,
   as_user: true,
   link_names: true,
-  text: 'Welcome to the team! We\'re glad you\'re here.',
-  attachments: JSON.stringify([
+  text: 'Welcome to the Accord Project! We\'re glad you\'re here.',
+  attachments: [
     {
-      title: 'What is Slack?',
-      text: 'Slack is where work happens. If this is your first time using Slack, take some time to read the help docs at get.slack.help and our internal wiki. If you have any questions, jump into #help-slack and we\'ll help you out',
+      title: 'What is the Accord Project?',
+      text: 'Industry-Led Specification and Open Source Code for Smart Legal Contracts. Find out more at accordproject.org and see the documentation at docs.accordproject.org',
       color: '#74c8ed',
     },
     {
       title: 'Code of Conduct',
-      text: 'Our goal is to maintain a safe, helpful and friendly community for everyone, regardless of experience, gender identity and expression, sexual orientation, disability, personal appearance, body size, race, ethnicity, age, religion, nationality, or other defining characteristic. Please take the time to read through <https://code.localhost|Code of Conduct> before continuing.',
-      callback_id: 'terms-of-service',
+      text: 'Help us keep the Accord Project open and inclusive. Please read and follow our <https://github.com/accordproject/docs/blob/master/Accord%20Project%20Code%20of%20Conduct.pdf|Code of Conduct> before continuing.',
       color: '#3060f0',
-      actions: [{
-        name: 'accept',
-        text: 'Accept',
-        type: 'button',
-        value: 'accept',
-        style: 'primary',
-      }],
-    }]),
+    },
+    {
+      title: 'Channels',
+      color: '#2243a5',
+      text: `We use slack channels to group related conversations for the different Accord Project working groups (WGs). Each working group has a chairperson who schedules regular meetings with members. 
+
+To join, simply introduce yourself in the corresponding channel.`
+    },
+    {
+      title: 'Financial Services',
+      color: '#edcd5a',
+      text: '#financialservices-wg. https://accordproject.org/working-groups/financial-services'
+    },
+    {
+      title: 'Supply Chain & Logistics ',
+      color: '#edcd5a',
+      text: '#supplychain-wg. https://accordproject.org/working-groups/supply-chain'
+    },
+        {
+      title: 'Intellectual Property',
+      color: '#edcd5a',
+      text: '#ip-wg. https://accordproject.org/working-groups/ip'
+    },
+    {
+      title: 'Venture and Token Sales',
+      color: '#edcd5a',
+      text: '#ventureandtokens-wg. https://accordproject.org/working-groups/venture'
+    },
+    {
+      title: 'Open Data & Standards',
+      color: '#edcd5a',
+      text: '#opendatastandards-wg'
+    },
+    {
+      title: 'Technology',
+      color: '#86d67c',
+      text: `#technology-wg. The technology working group builds the Accord Project's open-source software. 
+
+There are also several sub-groups for projects (#technology-cicero, #technology-ergo, #technology-models) and technologies (#tech-hyperledger & #technology-corda).`
+    },
+  ],
 };
 
 const initialMessage = (teamId, userId) => {
@@ -47,6 +79,7 @@ const initialMessage = (teamId, userId) => {
 
     // send the default message as a DM to the user
     message.channel = userId;
+    message.attachments = JSON.stringify(message.attachments);
     const params = qs.stringify(message);
     const sendMessage = axios.post('https://slack.com/api/chat.postMessage', params);
     sendMessage.then(postResult);
@@ -55,30 +88,30 @@ const initialMessage = (teamId, userId) => {
   }
 };
 
-// set the team/user record to true to indicate that they've accepted the ToS
-// you might want to store the date/time that the terms were accepted
+// // set the team/user record to true to indicate that they've accepted the ToS
+// // you might want to store the date/time that the terms were accepted
 
-const accept = (userId, teamId) => db.push(`/${teamId}/${userId}`, true);
+// const accept = (userId, teamId) => db.push(`/${teamId}/${userId}`, true);
 
-// find all the users who've been presented the ToS and send them a reminder to accept.
-// the same logic can be applied to find users that need to be removed from the team
-const remind = () => {
-  try {
-    const data = db.getData('/');
-    Object.keys(data).forEach((team) => {
-      Object.keys(data[team]).forEach((user) => {
-        if (!data[team][user]) {
-          message.channel = user;
-          message.text = 'REMIND I am a test message';
+// // find all the users who've been presented the ToS and send them a reminder to accept.
+// // the same logic can be applied to find users that need to be removed from the team
+// const remind = () => {
+//   try {
+//     const data = db.getData('/');
+//     Object.keys(data).forEach((team) => {
+//       Object.keys(data[team]).forEach((user) => {
+//         if (!data[team][user]) {
+//           message.channel = user;
+//           message.text = 'REMIND I am a test message';
 
-          const params = qs.stringify(message);
-          const sendMessage = axios.post('https://slack.com/api/chat.postMessage', params);
+//           const params = qs.stringify(message);
+//           const sendMessage = axios.post('https://slack.com/api/chat.postMessage', params);
 
-          sendMessage.then(postResult);
-        }
-      });
-    });
-  } catch (error) { console.error(error); }
-};
+//           sendMessage.then(postResult);
+//         }
+//       });
+//     });
+//   } catch (error) { console.error(error); }
+// };
 
-module.exports = { initialMessage, accept, remind };
+module.exports = { initialMessage, message };
